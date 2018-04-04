fa128f3a4f6373c4a7dbec957110f15fe51125bb
Morten Birk <mortenhbirk@gmail.com>
2018-04-04 12:30:29 +0200

    Separate graph loading and data visualization into different components

---
+++ src/visualize/VisualizationContainer.js
@@ -0,0 +1,95 @@
+"use strict";
+
+import React from 'react';
+import * as d3 from 'd3';
+
+import ForceSimulation from './ForceSimulation';
+
+function id() {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
+
+export default class VisualizationContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.uuid = "bearshark" + id();
+    this.state = {
+      svg: null
+    }
+  }
+
+  componentDidMount() {
+    this.svg = d3.select("#" + this.uuid)
+      .attr("width", this.props.width)
+      .attr("height", this.props.height);
+
+    //add encompassing group for the zoom
+    let everything = this.svg.append("g")
+      .attr("class", "everything");
+
+    //add zoom capabilities
+    let zoom_handler = d3.zoom()
+        .on("zoom", () => {
+          everything.attr("transform", d3.event.transform)
+        });
+
+    zoom_handler(this.svg);
+
+    // Define the arrow at the end of links.
+    everything.append("defs").selectAll("marker")
+      .data(["suit"])
+      .enter().append("marker")
+        .attr("id", function(d) { return d; })
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 0)
+        .attr("refY", 0)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+      .append("path")
+        .attr("d", "M0,-5L10,0L0,5 L10,0 L0, -5")
+        .style("stroke", "black")
+        .style("opacity", "0.6");
+
+    this.setState({
+      svg: everything,
+    });
+  }
+
+  componentDidUpdate() {
+    this.svg
+      .attr("width", this.props.width)
+      .attr("height", this.props.height);
+  }
+
+  render() {
+    const content = this.state.svg
+      ?
+        <ForceSimulation
+          svg={this.state.svg}
+          width={this.props.width}
+          height={this.props.height}
+          graph={this.props.graph}/>
+      :
+        "";
+    return (
+      <div>
+        <svg
+          id={this.uuid}
+          style= {{
+            border: "1px solid black",
+            backgroundColor: "#fff"
+          }}
+          width={this.props.width}
+          height={this.props.height}>
+        </svg>
+        {content}
+      </div>
+
+    );
+  }
+}
--- src/visualize/WrapperComponent.js
+++ src/visualize/WrapperComponent.js
@@ -3,22 +3,13 @@
 import React from 'react';
 import * as d3 from 'd3';
 
-import ForceSimulation from './ForceSimulation';
-
-function id() {
-  // Math.random should be unique because of its seeding algorithm.
-  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-  // after the decimal.
-  return '_' + Math.random().toString(36).substr(2, 9);
-};
+import VisualizationContainer from './VisualizationContainer';
 
 export default class WrapperComponent extends React.Component {
 
   constructor(props) {
     super(props);
-    this.uuid = "bearshark" + id();
     this.state = {
-      svg: null,
       graph: null
     }
   }
@@ -31,63 +22,17 @@ export default class WrapperComponent extends React.Component {
         console.log("Bearshark dependency tree has been fetched");
       }
     });
-
-    this.svg = d3.select("#" + this.uuid)
-      .attr("width", this.props.width)
-      .attr("height", this.props.height);
-
-    //add encompassing group for the zoom
-    let everything = this.svg.append("g")
-      .attr("class", "everything");
-
-    //add zoom capabilities
-    let zoom_handler = d3.zoom()
-        .on("zoom", () => {
-          everything.attr("transform", d3.event.transform)
-        });
-
-    zoom_handler(this.svg);
-
-    // Define the arrow at the end of links.
-    everything.append("defs").selectAll("marker")
-      .data(["suit"])
-      .enter().append("marker")
-        .attr("id", function(d) { return d; })
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 0)
-        .attr("refY", 0)
-        .attr("markerWidth", 6)
-        .attr("markerHeight", 6)
-        .attr("orient", "auto")
-      .append("path")
-        .attr("d", "M0,-5L10,0L0,5 L10,0 L0, -5")
-        .style("stroke", "black")
-        .style("opacity", "0.6");
-
-    this.setState({
-      svg: everything,
-    });
-  }
-
-  componentDidUpdate() {
-    this.svg
-      .attr("width", this.props.width)
-      .attr("height", this.props.height);
   }
 
   rebuild = () => {
     console.log("Reloading bearshark dependency tree")
-
     // Generate the reload url
     let url = "http://localhost:3462/rebuild"
     if (this.props.url) {
       url = this.props.url.replace("/id/", "/rebuild/")
     }
-
-    console.log(url);
     d3.json(url, (error, graph) => {
       this.setState({graph: graph});
-      console.log(graph);
       if (graph) {
         console.log("Bearshark dependency tree has been reloaded");
       }
@@ -95,10 +40,9 @@ export default class WrapperComponent extends React.Component {
   }
 
   render() {
-    const content = this.state.svg && this.state.graph
+    const content = this.state.graph
       ?
-        <ForceSimulation
-          svg={this.state.svg}
+        <VisualizationContainer
           width={this.props.width}
           height={this.props.height}
           graph={this.state.graph}/>
@@ -109,15 +53,9 @@ export default class WrapperComponent extends React.Component {
         style={{
           position: "relative"
         }}>
-        <svg
-          id={this.uuid}
-          style= {{
-            border: "1px solid black",
-            backgroundColor: "#fff"
-          }}
-          width={this.props.width}
-          height={this.props.height}>
-        </svg>
+
+        {content}
+
         <div
           style={{
             position: "absolute",
@@ -131,7 +69,6 @@ export default class WrapperComponent extends React.Component {
           onClick={this.rebuild}>
           Rebuild
         </div>
-        {content}
       </div>
 
     );
